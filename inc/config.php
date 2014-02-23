f8eafedf6689db6497a1f25ef88f9aa18e23afa8
Ian Stacey <ian.stacey@gmail.com>
2014-02-23 22:11:00 +0000

    Carried on working on shouting

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?php
+//$baseUrl = 'http://www.buttyrunner.com/';
+$baseUrl = 'http://buttyrunner.istacey.eu.local/';
+
+$db_server = '127.0.0.1';
+$db_user = 'buttyrunner';
+$db_password = 'fVhxUZGM8auZ8neK';
+$db_database = 'buttyrunner';	
+?>
--- shout/index.php
+++ shout/index.php
@@ -1,31 +1,58 @@
 <?php
-if( count( $_POST ) > 0){
-	
+require_once('../inc/config.php');
+
+if(count( $_POST) > 0)
+{	
 	// Store order in database
+	$mysqli = new mysqli($db_server, $db_user, $db_password, $db_database);
 	
-	$baseUrl = 'http://www.buttyrunner.com/';
-	$id = '3289723089423790237';
+	// If email address doesn't already exist, create the user
+	$result = $mysqli->query('SELECT id, name FROM user WHERE email = "' . trim($_POST['email']) . '" LIMIT 1');
+	if($result->num_rows > 0)
+	{
+		$row = $result->fetch_row();
+		$user_id = $row['0'];
+		
+		// If the user's name has changed (why?) keep the data current
+		if($row['1'] != trim($_POST['name']))
+		{
+			$mysqli->query('UPDATE user SET name = "' . $_POST['name'] . '" WHERE id = "' . $row[0] . '"');
+		}
+	}
+	else
+	{
+		$user_id = uniqid("1");
+		$mysqli->query('INSERT INTO user (id, name, email) VALUES ("' . $user_id . '", "' . trim($_POST['name']) . '", "' . trim($_POST['email']) . '")');		
+	}
+	$result->close();
+		
+	// Create the new order	
+	$order_id = uniqid("2");
+	$collect = DateTime::createFromFormat('d/m/Y H:i', $_POST["collect_date"] . ' ' . $_POST["collect_time"]);
+	$deadline = DateTime::createFromFormat('d/m/Y H:i', $_POST["collect_date"] . ' ' . $_POST["collect_time"]);
+	$deadlineInterval = new DateInterval('PT' . $_POST["deadline"] . 'M');
+	$deadline->sub($deadlineInterval);
+	$mysqli->query('INSERT INTO buttyrun (id, user_id, vendor, collect, deadline) VALUES ("' . $order_id . '", "' . $user_id . '", "' . $_POST['vendor'] . '", "' . $collect->format('Y-m-d H:i:s') . '", "' . $deadline->format('Y-m-d H:i:s') . '")');
+	
+	// Add My Item
+	
+	$mysqli->close();
 	
 	// Send email to organiser
-	
-	$subject = 'Your Butty Run to ' . trim($_POST['where']) . ' on ' . trim($_POST['whenDate']) . ' at ' . trim($_POST['whenTime']); 
+	$subject = 'Your Butty Run to ' . trim($_POST['vendor']) . ' on ' . trim($_POST['collect_date']) . ' at ' . trim($_POST['collect_time']); 
 	$headers = 'From: noreply@buttyrunner.com' . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-	$message = 'Hi ' . trim($_POST['name']) . ','. "\r\n\r\n" . 'You\'re going to ' . trim($_POST['where']) .' on ' . trim($_POST['whenDate']) . ' at ' . trim($_POST['whenTime']) . '.' . "\r\n\r\n" . 'The ButtyList is available at the link below.'. "\r\n\r\n" . $baseUrl . 'list/?' . $id . "\r\n\r\n" . 'Enjor your food!' . "\r\n\r\n" . '--' . "\r\n". 'ButtyRunner.com' . "\r\n". 'Follow us on Twitter at http://twitter.com/buttyrunner'; 
-	
+	$message = 'Hi ' . trim($_POST['name']) . ','. "\r\n\r\n" . 'You\'re going to ' . trim($_POST['vendor']) .' on ' . trim($_POST['collect_date']) . ' at ' . trim($_POST['collect_time']) . '.' . "\r\n\r\n" . 'The ButtyList is available at the link below.'. "\r\n\r\n" . $baseUrl . 'list/?' . $order_id . "\r\n\r\n" . 'Enjor your food!' . "\r\n\r\n" . '--' . "\r\n". 'ButtyRunner.com' . "\r\n". 'Follow us on Twitter at http://twitter.com/buttyrunner'; 	
 	mail(trim($_POST['email']), $subject, $message, $headers);
 	
-	// Send email to invetees
-	
-	$subject = 'ButtyRun to ' . trim($_POST['where']) . ' on ' . trim($_POST['whenDate']) . ' at ' . trim($_POST['whenTime']);	
-	$message = 'Hi,'. "\r\n\r\n" . 'I\'m going to ' . trim($_POST['where']) . ' on ' . trim($_POST['whenDate']) . ' at ' . trim($_POST['whenTime']) . "\r\n\r\n" . 'You can place your order at the link below.'. "\r\n\r\n" . $baseUrl . 'order/?' . $id . "\r\n\r\n" . trim($_POST['name']) . "\r\n\r\n" . '--' . "\r\n". 'ButtyRunner.com' . "\r\n". 'Follow us on Twitter at http://twitter.com/buttyrunner';	
+	// Send email to invetees	
+	$subject = 'ButtyRun to ' . trim($_POST['vendor']) . ' on ' . trim($_POST['collect_date']) . ' at ' . trim($_POST['collect_time']);	
+	$message = 'Hi,'. "\r\n\r\n" . 'I\'m going to ' . trim($_POST['vendor']) . ' on ' . trim($_POST['collect_date']) . ' at ' . trim($_POST['collect_time']) . '.' . "\r\n\r\n" . 'You can place your order at the link below until ' . $deadline->format('H:i') . '.'. "\r\n\r\n" . $baseUrl . 'order/?' . $order_id . "\r\n\r\n" . trim($_POST['name']) . "\r\n\r\n" . '--' . "\r\n". 'ButtyRunner.com' . "\r\n". 'Follow us on Twitter at http://twitter.com/buttyrunner';	
 	$headers = 'From: '. trim($_POST['email']) . "\r\n" . 'X-Mailer: PHP/' . phpversion();
-	
 	$invitees = explode("\r\n", trim($_POST['shout']));
 	foreach($invitees as $invite){
 		mail(trim($invite), $subject, $message, $headers);
-	}
-	
-	header('Location: ' . $baseUrl . 'list/?' . $id);
+	}	
+	header('Location: ' . $baseUrl . 'list/?' . $order_id);
 	exit();
 }
 ?><!DOCTYPE html>
@@ -88,13 +115,13 @@ if( count( $_POST ) > 0){
 					                    </div>
 					                    
 					                    <div class="form-group">
-					                      <label for="where" class="col-lg-2 control-label">Where</label>
+					                      <label for="vendor" class="col-lg-2 control-label">Where</label>
 					                      <div class="col-lg-10">
-					                        <select class="form-control" id="where" name="where">
+					                        <select class="form-control" id="vendor" name="vendor">
 												<option value="-1">Please select</option>
-											  <option value="The Pantry">The Pantry</option>
-					                          <option value="Costa">Costa</option>
-					                          <option value="Subway">Subway</option>
+											  <option>The Pantry</option>
+					                          <option>Costa</option>
+					                          <option>Subway</option>
 					                        </select>					                        
 					                      </div>
 									    </div>
@@ -108,22 +135,22 @@ if( count( $_POST ) > 0){
   					                    </div>
 										
 										<div class="form-group">
-											<label for="whenDate" class="col-lg-2 control-label">Date</label>
+											<label for="collect_date" class="col-lg-2 control-label">Date</label>
   					                          <div class="col-lg-10">
-						                        <input type="text" class="form-control" id="whenDate" placeholder="dd/mm/yyyy" name="whenDate">
+						                        <input type="text" class="form-control" id="collect_date" placeholder="dd/mm/yyyy" name="collect_date">
 	  											</div>
 											</div>
 											<div class="form-group">
-												<label for="whenTime" class="col-lg-2 control-label">Time</label>
+												<label for="collect_time" class="col-lg-2 control-label">Time</label>
 											<div class="col-lg-10">
-  						                        <input type="text" class="form-control" id="whenTime" placeholder="hh:mm" name="whenTime">
+  						                        <input type="text" class="form-control" id="collect_time" placeholder="hh:mm" name="collect_time">
 	  											</div>
 										</div>
 										
 					                    <div class="form-group">
-					                      <label for="ordersBy" class="col-lg-2 control-label">Deadline</label>
+					                      <label for="deadline" class="col-lg-2 control-label">Deadline</label>
 					                      <div class="col-lg-10">
-					                        <select class="form-control" id="ordersBy" name="ordersBy">
+					                        <select class="form-control" id="deadline" name="deadline">
 											<option value="-1">Please select</option>
 											  <option value="5">5 minutes before</option>
 					                          <option value="15">15 minutes before</option>
